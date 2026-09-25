7d25ea4a4ae41ce5726d5e297aac9c480354b4ac
EricStephen12 <deamirclothingstores@gmail.com>
2026-09-25 19:31:36 +0100

    fixed

--- src/components/rehearsal/RehearsalSongRow.tsx
+++ src/components/rehearsal/RehearsalSongRow.tsx
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { Image } from 'expo-image';
 
-const TRACK_PLACEHOLDER = require('../../assets/TRACK_PLACEHOLDER.png');
+const TRACK_PLACEHOLDER = require('../../../assets/TRACK_PLACEHOLDER.png');
 
 interface Props {
   track: any;
